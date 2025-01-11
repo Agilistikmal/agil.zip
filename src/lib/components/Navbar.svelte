4957996fadf5b49bc2c853d4004171e3a3b54acc
agilistikmal <agilistikmal3@gmail.com>
2025-01-11 14:46:11 +0700

    add: Navbar menu

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,5 +1,16 @@
 <script lang="ts">
 	import Icon from '@iconify/svelte';
+	import NavbarMenu from './NavbarMenu.svelte';
+	import { isMenuOpen } from '$lib/state/navbar';
+	import { clickOutside } from '$lib/utils/clickOutside';
+
+	function setIsMenuOpen() {
+		isMenuOpen.update((value) => !value);
+	}
+
+	function closeMenu() {
+		isMenuOpen.update((value) => false);
+	}
 </script>
 
 <nav class="fixed left-0 top-0 w-full backdrop-blur-3xl z-50">
@@ -8,9 +19,15 @@
 			<a href="/" data-sveltekit-preload-data data-sveltekit-preload-code>
 				<img src="/logo/agil.zip.white.png" alt="agil.zip.white.png" class="h-8" />
 			</a>
-			<div class="flex items-center gap-2">
-				<span>Menu</span>
-				<Icon icon="duo-icons:app" class="text-3xl" />
+			<div id="navbarMenu" class="relative" use:clickOutside on:clickOutside={closeMenu}>
+				<button class="flex items-center gap-2" on:click={setIsMenuOpen}>
+					<span>Menu</span>
+					<Icon icon="duo-icons:app" class="text-3xl" />
+				</button>
+
+				{#if $isMenuOpen}
+					<NavbarMenu />
+				{/if}
 			</div>
 		</div>
 	</div>
---
+++ src/lib/components/NavbarMenu.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+	import Icon from '@iconify/svelte';
+	import { fly } from 'svelte/transition';
+</script>
+
+<div class="absolute top-12 right-0 w-72" transition:fly={{ duration: 500, y: -20 }}>
+	<div class="bg-white/10 backdrop-filter backdrop-blur-sm p-5">
+		<ul class="space-y-4">
+			<li class="group">
+				<a href="/" class="flex items-center gap-1">
+					<Icon
+						icon="uim:web-grid"
+						width="24"
+						height="24"
+						class="group-hover:text-colombiablue transition duration-500"
+					/>
+					<span class="group-hover:translate-x-2 transition duration-500">Home</span>
+				</a>
+			</li>
+			<li class="group">
+				<a href="/about" class="flex items-center gap-1">
+					<Icon
+						icon="uim:document-layout-center"
+						width="24"
+						height="24"
+						class="group-hover:text-colombiablue transition duration-500"
+					/>
+					<span class="group-hover:translate-x-2 transition duration-500">About</span>
+				</a>
+			</li>
+			<li class="group">
+				<a href="/extract" class="flex items-center gap-1">
+					<Icon
+						icon="uim:layers-alt"
+						width="24"
+						height="24"
+						class="group-hover:text-colombiablue transition duration-500"
+					/>
+					<span class="group-hover:translate-x-2 transition duration-500">
+						Extract, view project
+					</span>
+				</a>
+			</li>
+		</ul>
+	</div>
+</div>
